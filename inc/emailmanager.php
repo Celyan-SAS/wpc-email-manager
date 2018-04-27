c0e67825bcf264a5395e8b326df4da22f1885918
ydubois <yann@abc.fr>
2018-04-27 11:06:07 +0200

    Style

--- inc/emailmanager.php
+++ inc/emailmanager.php
@@ -114,15 +114,15 @@ class WPC_mail {
 				<h2><?php echo apply_filters( 'wpc_email_live_title', __('LIVE email','wpc_emailmanager') ); ?></h2>
 			
 				<div>
-					<span><?php echo __('Recipients:','');?></span>
+					<span class="h-item"><?php echo __('Recipients:','');?></span>
 					<input type="text" name="toemail" value="<?php echo $email_infos['to']; ?>">
 				</div>
 				<div>
-					<span><?php echo __('Subject:','');?></span>
+					<span class="h-item"><?php echo __('Subject:','');?></span>
 					<input type="text" name="subjectemail" value="<?php echo $email_infos['subject']; ?>">
 				</div>
 				<div>
-					<span><?php echo __('Body:','');?></span>
+					<span class="h-item"><?php echo __('Body:','');?></span>
 					<?php
 					$editor_options = array(
 					  'media_buttons'=>true,
